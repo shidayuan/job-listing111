a569d10cec216e6a1bb84b02acf1e5dd14a068b9
shichengyuan <15036062755@163.com>
2018-03-22 10:49:32 +0800

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
 
   def require_is_admin
-   if !current_user.admin?
-     flash[:alert] = 'You are not admin'
-     redirect_to root_path
-   end
- end
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,8 +2,6 @@ class Job < ApplicationRecord
   validates :title, presence: true
 
 
-  def admin?
-    is_admin
-  end
+  
 
 end
